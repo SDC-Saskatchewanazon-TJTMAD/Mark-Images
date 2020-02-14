76e757d5472c6a5ebffb1debcf7c9b4a1f487976
Mark Wallace <mwallace999@gmail.com>
2020-02-14 12:21:12 -0600

    Refactored for recursion

--- server/mongoSeeder.js
+++ server/mongoSeeder.js
@@ -2,54 +2,63 @@
 // un by typing command: node server/mongoSeeder.js FROM project folder
 
 const faker = require('faker');
-const MongoClient = require("mongodb").MongoClient;
+const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 const _ = require('lodash');
 
 // Connection URL
 const url = 'mongodb://localhost:27017';
 
+
 // Database Name
 const dbName = 'test'; //*** Change to Database name ***
 const collectionName = 'testProductData'; //*** Change to Collection name ***/
-const dataBlockSize = 10; //*** Size of each Array that is pushed to db ***/
+const dataBlockSize = 100; //*** Size of each Array that is pushed to db ***/
+let db;
+let counter = 1;
+const quantity = 1000;
 
-const mongoSeeder = (quantity, collection, overwrite) => {
-  // Use connect method to connect to the server
+// Create data instances
+const seed = (collection) => {
+  let products = [];
+  for (let i = counter; i < counter + dataBlockSize; i++) {
+    const productName = faker.commerce.productName();
+    const newProduct = {
+      productId: i,
+      productName,
+      image: 'https://picsum.photos/640/480',
+    };
+    products.push(newProduct);
+    console.log(newProduct.productId, newProduct.productName, newProduct.image);
+  }
+  db.collection(collection).insertMany(products)
+    .then(products = [])
+    //.then(console.log(`Partial seed completed ${Math.floor(counter / dataBlockSize)} of ${Math.floor(quantity / dataBlockSize)}`))
+    .then(() => {
+      if (counter + dataBlockSize <= quantity) {
+        counter+=dataBlockSize;
+        seed(collection);
+      } else {
+        console.log('Database seeded!');
+      }
+    });
+};
+
+// Delete database and run seed
+const mongoSeeder = (collection, overwrite) => {
   MongoClient.connect(url, (err, client) => {
     assert.equal(null, err);
-
-    const db = client.db(dbName);
-
-    if (overwrite /*&& db.collection(collection)*/) {
-      db.collection(collection).drop((err, del) => {
-        // Error Handling
-        if (err) throw err;
+    db = client.db(dbName);
+    if (overwrite && db.collection(collection)) {
+      db.collection(collection).drop((error, del) => {
+        if (error) throw error;
         if (del) console.log('Collection deleted');
-
-        // Create data instances
-        let products = [];
-        let counter = 1;
-        for (let i = 1; i <= quantity; i++) {
-          const productName = faker.commerce.productName();
-          const newProduct = {
-            productId: i,
-            productName,
-            image: 'https://picsum.photos/640/480',
-          };
-          products.push(newProduct);
-          console.log(newProduct.productId, newProduct.productName, newProduct.image);
-          if (products.length === dataBlockSize || i === quantity) {
-            db.collection(collection).insertMany(products)
-              .then(products = [])
-              .then(console.log(`Partial seed completed ${counter} of ${Math.floor(quantity / dataBlockSize)}`))
-              .then(counter++);
-          }
-        }
-        console.log('Database seeded!');
-        client.close();
+        seed(collection);
       });
+    } else {
+      seed(collection);
     }
   });
 };
-mongoSeeder(100, collectionName, false);
+
+mongoSeeder(collectionName, true);
